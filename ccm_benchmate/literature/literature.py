cc942abda1d4e75b25619e0261f9f3d3d85051be
alper <celikalper84@gmail.com>
2025-09-03 12:22:15 -0400

    recursion, getting paper relevance (any text really) some minor refactoring to allow it all.

--- ccm_benchmate/literature/literature.py
+++ ccm_benchmate/literature/literature.py
@@ -1,7 +1,8 @@
 import os.path
-import warnings
+import time
+
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Optional
 
 import numpy as np
 from bs4 import BeautifulSoup as bs
@@ -11,15 +12,55 @@ from ccm_benchmate.literature.utils import *
 class NoPapersError(Exception):
     pass
 
+def paper_from_response(openalex_response):
+    if "pmid" in openalex_response["ids"].keys():
+        paper_id=openalex_response["ids"]["pmid"].split("/").pop()
+        id_type="pubmed"
+    else:
+        raise ValueError("Could not find a valid paper ID in the response.")
+
+    paper=Paper(paper_id=paper_id, id_type=id_type, search_info=False, download=False, process=False)
+    paper.info.openalex_info = filter_openalex_response(openalex_response)
+    if "best_oa_location" in openalex_response.keys() and openalex_response["best_oa_location"] is not None:
+        link = openalex_response["best_oa_location"]["pdf_url"]
+        if link is not None and link.endswith(".pdf"):
+            download_link = openalex_response["best_oa_location"]["pdf_url"]
+        else:
+            warnings.warn("Did not find a direct pdf download link")
+            download_link = None
+    else:
+        warnings.warn("There is no place to download the paper, this paper might not be open access")
+        download_link = None
+    paper.info.download_link = download_link
+    return paper
+
+
+def paper_from_link(link):
+    openalex_id=link.split("/").pop()
+    info=search_openalex(paper_id=openalex_id, id_type="openalex")
+    paper=paper_from_response(info)
+    return paper
+
 class LitSearch:
-    def __init__(self, pubmed_api_key=None):
+    def __init__(self, pubmed_api_key=None, email=None, sort_by="relevance"):
         """
         create the ncessary framework for searching
         :param pubmed_api_key:
         """
         self.pubmed_key = pubmed_api_key
+        self.email=email
+        if sort_by not in ["relevance", "pub+date"]:
+            raise ValueError("sort_by must be relevance or pub+date")
+        self.sorting=sort_by
+        self.params={
+            "retmode": "xml",
+            "email": self.email,
+            "api_key": self.pubmed_key,
+            "sort": self.sorting,
+        }
 
-    #TODO advanced search, while technically supported because query is just a string it would be nice if it was explicit
+
+        #TODO advanced search, while technically supported because query is just a string it would be nice if it was explicit
     def search(self, query, database="pubmed", results="id", max_results=1000):
         """
         search pubmed and arxiv for a query, this is just keyword search no other params are implemented at the moment
@@ -29,10 +70,10 @@ class LitSearch:
         :param max_results:
         :return: paper ids specific to the database
         """
-        #TODO implement pubmed api key for non-free papers
+        #TODO implement pubmed api key for non-free papers, implement email
         if database == "pubmed":
             search_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={}&retmax={}".format(query, max_results)
-            search_response = requests.get(search_url)
+            search_response = requests.get(search_url, params=self.params)
             search_response.raise_for_status()
 
             soup = bs(search_response.text, "xml")
@@ -66,26 +107,28 @@ class PaperInfo:
     id_type: str
     title: Optional[str] = None
     abstract: Optional[str] = None
-    abstract_sentences: Optional[list] = None
     abstract_embeddings: Optional[np.ndarray]  = None
     text: Optional[str] = None
     text_chunks: Optional[list] = None
     chunk_embeddings: Optional[np.ndarray] = None
     figures: Optional[list] = None
+    figure_embeddings: Optional[np.ndarray] = None
     tables: Optional[list] = None
+    table_embeddings: Optional[np.ndarray] = None
     figure_interpretation: Optional[str] = None
     table_interpretation: Optional[str] = None
     figure_interpretation_embeddings: Optional[np.ndarray] = None
     table_interpretation_embeddings: Optional[np.ndarray] = None
     download_link: str = None
-    downloaded: bool = False
     pathname: str = None
+    openalex_info: Optional[dict] = None
+
 
 
 
 class Paper:
-    def __init__(self, paper_id, id_type="pubmed", search_info=True, citations=True,
-                 references=True, related_works=True, download=True, destination=".", process=True, **process_kwargs):
+    def __init__(self, paper_id, id_type="pubmed", search_info=True, download=True,
+                 destination=".", process=True, **process_kwargs):
         """
         This class is used to download and process a paper from a given id, it can also be used to process a paper from a file
         :param paper_id:
@@ -99,22 +142,8 @@ class Paper:
         self.info.abstract, self.info.title = self.get_abstract()
 
         if search_info:
-            self.paper_info = search_openalex(id_type=self.info.id_type, paper_id=self.info.id,
-                                              cited_by=citations,
-                                              references=references, related_works=related_works)
-            if self.paper_info is None:
-                raise NoPapersError("Could not find a paper with id {}".format(self.info.id))
+            self.info.openalex_info, self.info.download_link = self.search_info()
 
-            if "best_oa_location" in self.paper_info.keys() and self.paper_info["best_oa_location"] is not None:
-                link = self.paper_info["best_oa_location"]["pdf_url"]
-                if link is not None and link.endswith(".pdf"):
-                    self.info.download_link = self.paper_info["best_oa_location"]["pdf_url"]
-                else:
-                    warnings.warn("Did not find a direct pdf download link")
-                    self.info.download_link = None
-            else:
-                warnings.warn("There is no place to download the paper, this paper might not be open access")
-                self.info.download_link = None
         if download and self.info.download_link is not None:
             try:
                 self.download(destination)
@@ -123,6 +152,7 @@ class Paper:
             except:
                 self.info.downloaded=False
                 warnings.warn("Could not download paper")
+
         if process and self.info.downloaded:
             self.process(self.info.pathname, **process_kwargs)
 
@@ -134,7 +164,11 @@ class Paper:
             response=requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={}".format(self.info.id))
             response.raise_for_status()
             soup=bs(response.text, "xml")
-            abstract_text=soup.find("AbstractText").text
+            abstract_text=soup.find("AbstractText")
+            if abstract_text is not None:
+                abstract_text=abstract_text.text
+            else:
+                abstract_text=None
             title=soup.find("ArticleTitle").text
 
         elif self.info.id_type == "arxiv":
@@ -153,6 +187,28 @@ class Paper:
 
         return abstract_text, title
 
+    def search_info(self):
+        openalex_info = search_openalex(id_type=self.info.id_type, paper_id=self.info.id)
+        if openalex_info is None:
+            warnings.warn("Could not find a paper with id {}".format(self.info.id))
+
+        else:
+            if "best_oa_location" in openalex_info.keys() and openalex_info[
+                "best_oa_location"] is not None:
+                link = openalex_info["best_oa_location"]["pdf_url"]
+                if link is not None and link.endswith(".pdf"):
+                    download_link = openalex_info["best_oa_location"]["pdf_url"]
+                else:
+                    warnings.warn("Did not find a direct pdf download link")
+                    download_link = None
+            else:
+                warnings.warn("There is no place to download the paper, this paper might not be open access")
+                download_link = None
+
+        self.info.openalex_info=openalex_info
+        self.info.download_link=openalex_info
+        return None
+
     def download(self, destination):
         download = requests.get(self.info.download_link, stream=True)
         download.raise_for_status()
@@ -160,7 +216,7 @@ class Paper:
             f.write(download.content)
         file_paths=os.path.abspath(os.path.join("{}/{}.pdf".format(destination, self.info.id)))
         self.info.pathname=file_paths
-        return self
+        return None
 
     #TODO I need to pass arguments properly the **kwargs is not going to work
     def process(self, file_path, embed_images=True, embed_text=True,
@@ -204,10 +260,66 @@ class Paper:
                                                                       splitting_strategy="none",
                                                                      **kwargs)[1]
 
-        return self
+        return None
+
+    def get_references(self):
+        if "referenced_works" not in self.info.openalex_info.keys():
+            raise ValueError("The response does not contain references.")
+        references=self.info.openalex_info["referenced_works"]
+        papers=[]
+        for reference in references:
+            try:
+                p=paper_from_link(reference)
+                papers.append(p)
+                time.sleep(0.1)
+            except:
+                print("Could not find a paper with id {}".format(reference.split("/").pop()))
+
+        self.info.references=papers
+        return None
+
+    def get_related_works(self):
+        if "related_works" not in self.info.openalex_info.keys():
+            raise ValueError("The response does not contain related works.")
+        references = self.info.openalex_info["related_works"]
+        papers = []
+        for reference in references:
+            try:
+                p = paper_from_link(reference)
+                papers.append(p)
+                time.sleep(0.1)
+            except:
+                print("Could not find a paper with id {}".format(reference.split("/").pop()))
+        self.info.related_works=papers
+        return None
+
+    def get_cited_by(self, cursor="*"):
+        if "cited_by_api_url" not in self.info.openalex_info.keys():
+            raise ValueError("The response does not contain cited by information.")
+        url = self.info.openalex_info["cited_by_api_url"] + "&cursor="
+        content = requests.get(url + cursor).content.decode().strip()
+        content = json.loads(content)
+        next_cursor = content["meta"]["next_cursor"]
+        papers = []
+        for item in content["results"] and len(content["results"])>0:
+            try:
+                p = paper_from_response(item)
+                papers.append(p)
+                time.sleep(0.1)
+            except:
+                print("Could not find a paper with id {}".format())
+            finally:
+                cursor=next_cursor
+        while next_cursor != cursor and next_cursor is not None:
+            self.get_cited_by(next_cursor)
+        self.info.cited_by=papers
+        return None
 
     def __str__(self):
         return self.info.title
+
+    def __repr__(self):
+        return "Paper(id={}, id_type={}, title={})".format(self.info.id, self.info.id_type, self.info.title)
 
 
 
